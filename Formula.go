5b084a0feb9cc518b2b4c8858b3e62411c2c553e
Christian Hovenbitzer <christian.hovenbitzer@gmx.net>
2019-03-12 10:58:29 +0100

    add formula

--- Formula.go
+++ Formula.go
@@ -1,5 +1,9 @@
 package excel
 
+import (
+	"fmt"
+)
+
 // Formula wraps the coords that will be contained by the formula in a struct
 type Formula struct {
 	Coords []Coordinates
@@ -8,9 +12,30 @@ type Formula struct {
 // Add adds the coords
 func (formula *Formula) Add() string {
 	str := "="
-	for idx, c := range formula.Coords {
+	for _, c := range formula.Coords {
 		str += c.ToString()
+		if c != formula.Coords[len(formula.Coords)-1] {
+			str += "+"
+		}
+	}
+	fmt.Println(str)
+	return str
+}
 
+// Substract substracts the provided coords. The minuend is defined by the function in parameter
+func (formula *Formula) Substract(fn func(coords []Coordinates) Coordinates) string {
+	min := fn(formula.Coords)
+	str := fmt.Sprintf("=%s", min.ToString())
+	for _, sub := range formula.Coords {
+		if sub.ToString() == min.ToString() {
+			continue
+		}
+		str += fmt.Sprintf("-%s", sub.ToString())
 	}
 	return str
+}
+
+// Raw provides the coords and expects a excel-ready string
+func (formula *Formula) Raw(fn func(coords []Coordinates) string) string {
+	return fn(formula.Coords)
 }
