0129ae80bef9c7c805a8cbdeac7339360d86142f
Empfang <empfang@Cecile-Casenobe.local>
2019-03-29 12:21:07 +0100

    add Coordinates() to Cell

--- Cell.go
+++ Cell.go
@@ -1,8 +1,18 @@
 package excel
 
+import "errors"
+
 // Cell wraps a cell's value and its style in a struct
 type Cell struct {
 	Value       interface{}
 	Style       Style
 	coordinates Coordinates
+}
+
+// Coordinates returns the coordinates associated with cell
+func (c *Cell) Coordinates() (Coordinates, error) {
+	if c.coordinates == (Coordinates{}) {
+		return Coordinates{}, errors.New("Coordinates for Cell are not yet initialized, returning empty stuct")
+	}
+	return c.coordinates, nil
 }
